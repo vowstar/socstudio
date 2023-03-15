fd6706cdd8a4477e6835e92818c6dc2617a0f1bb
Huang Rui <vowstar@gmail.com>
2023-03-15 18:24:51 +0800

    i18n/app_zh_CN.ts: add app translation
    Signed-off-by: Huang Rui <vowstar@gmail.com>

--- i18n/app_zh_CN.ts
+++ i18n/app_zh_CN.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../ui/mainwindow.ui" line="14"/>
         <source>SoC Studio</source>
-        <translation type="unfinished"></translation>
+        <translation>芯片工作室</translation>
     </message>
 </context>
 <context>
@@ -14,22 +14,22 @@
     <message>
         <location filename="../src/main.cpp" line="41"/>
         <source>SoC Studio</source>
-        <translation type="unfinished"></translation>
+        <translation>芯片工作室</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="44"/>
         <source>Generate SoC components via the command line.</source>
-        <translation type="unfinished"></translation>
+        <translation>通过命令行生成 SoC 组件。</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="50"/>
         <source>Start the software in CLI mode, otherwise it will start in GUI mode.</source>
-        <translation type="unfinished"></translation>
+        <translation>以CLI模式启动软件，否则将以GUI模式启动。</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="52"/>
         <source>Set log level. 0 is silent, 1 is error, 2 is warning, 3 is info, 4 is debug, 5 is verbose.</source>
-        <translation type="unfinished"></translation>
+        <translation>设置日志级别。 0 表示静默，1 表示错误，2 表示警告，3 表示信息，4 表示调试，5 表示详细。</translation>
     </message>
 </context>
 </TS>
