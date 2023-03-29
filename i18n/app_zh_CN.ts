928ab090880ded46979a88bb5a5f628495a9a2d6
Huang Rui <vowstar@gmail.com>
2023-03-30 00:02:04 +0800

    i18n/app_zh_CN.ts: update translations
    Signed-off-by: Huang Rui <vowstar@gmail.com>

--- i18n/app_zh_CN.ts
+++ i18n/app_zh_CN.ts
@@ -12,24 +12,61 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../src/main.cpp" line="41"/>
-        <source>SoC Studio</source>
-        <translation>芯片工作室</translation>
+        <location filename="../src/qsoccliworker.cpp" line="42"/>
+        <source>Generate SoC components via the command line</source>
+        <translation>通过命令行生成 SoC 组件</translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="44"/>
-        <source>Generate SoC components via the command line.</source>
-        <translation>通过命令行生成 SoC 组件。</translation>
+        <location filename="../src/qsoccliworker.cpp" line="48"/>
+        <source>Start the software in CLI mode, otherwise it will start in GUI mode</source>
+        <translation>以CLI模式启动软件，否则将以GUI模式启动</translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="50"/>
-        <source>Start the software in CLI mode, otherwise it will start in GUI mode.</source>
-        <translation>以CLI模式启动软件，否则将以GUI模式启动。</translation>
+        <location filename="../src/qsoccliworker.cpp" line="51"/>
+        <source>Set log level. 0 is silent, 1 is error, 2 is warning, 3 is info, 4 is debug, 5 is verbose</source>
+        <translation>设置日志级别。 0 表示静默，1 表示错误，2 表示警告，3 表示信息，4 表示调试，5 表示详细</translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="52"/>
-        <source>Set log level. 0 is silent, 1 is error, 2 is warning, 3 is info, 4 is debug, 5 is verbose.</source>
-        <translation>设置日志级别。 0 表示静默，1 表示错误，2 表示警告，3 表示信息，4 表示调试，5 表示详细。</translation>
+        <location filename="../src/qsoccliworker.cpp" line="59"/>
+        <source>gui         Start the software in GUI mode
+symbol      Import, update of symbol
+schematic   Processing of Schematic
+generate    Generate rtl, such as verilog, etc.
+</source>
+        <translation>gui         以GUI模式启动软件
+symbol      符号的导入、更新
+schematic   原理图的处理
+generate    生成rtl，比如verilog等
+</translation>
+    </message>
+    <message>
+        <location filename="../src/qsoccliworker.cpp" line="86"/>
+        <source>import   Import symbol from verilog
+update   Update symbol from verilog
+remove   Remove symbol by name</source>
+        <translation>import   从verilog导入符号
+update   从verilog更新符号
+remove   按名称移除符号</translation>
+    </message>
+    <message>
+        <location filename="../src/qsoccliworker.cpp" line="94"/>
+        <source>The verilog files to be processed</source>
+        <translation>要处理的verilog文件</translation>
+    </message>
+    <message>
+        <location filename="../src/qsoccliworker.cpp" line="98"/>
+        <source>The path where the file list is located, including a list of verilog files in order</source>
+        <translation>文件列表所在路径，包含顺序排列的verilog文件列表</translation>
+    </message>
+    <message>
+        <location filename="../src/qsoccliworker.cpp" line="106"/>
+        <source>The symbol name to be imported, updated or removed</source>
+        <translation>要导入、更新或删除的符号名称</translation>
+    </message>
+    <message>
+        <location filename="../src/qsoccliworker.cpp" line="124"/>
+        <source>The name of the symbol to be removed</source>
+        <translation>要删除的符号的名称</translation>
     </message>
 </context>
 </TS>
