22ff3e5915a7544b4b16f1f8aa307bc3bb74f294
Huang Rui <vowstar@gmail.com>
2023-06-12 22:25:02 +0800

    src/qsoccliparseproject.cpp: add project update function
    Signed-off-by: Huang Rui <vowstar@gmail.com>

--- src/qsoccliparseproject.cpp
+++ src/qsoccliparseproject.cpp
@@ -31,7 +31,7 @@ void QSocCliWorker::parseProject(const QStringList &appArguments)
         if (command == "create") {
             nextArguments.removeOne(command);
             parseProjectCreate(nextArguments);
-        } else if (command == "config") {
+        } else if (command == "update") {
             nextArguments.removeOne(command);
             parseProjectUpdate(nextArguments);
         } else if (command == "remove") {
@@ -83,12 +83,80 @@ void QSocCliWorker::parseProjectCreate(const QStringList &appArguments)
         } else {
             parser.showHelp(0);
         }
+        return;
+    }
+    /* Pass projectName to projectManager */
+    const QString     &projectName = cmdArguments.first();
+    QSocProjectManager projectManager;
+    if (parser.isSet("path")) {
+        projectManager.setProjectPath(parser.value("path"));
+    }
+    if (parser.isSet("bus")) {
+        projectManager.setBusPath(parser.value("bus"));
+    }
+    if (parser.isSet("sym")) {
+        projectManager.setSymbolPath(parser.value("sym"));
+    }
+    if (parser.isSet("sch")) {
+        projectManager.setSchematicPath(parser.value("sch"));
+    }
+    if (parser.isSet("output")) {
+        projectManager.setOutputPath(parser.value("output"));
+    }
+    if (projectManager.save(projectName)) {
+        qInfo() << "Project" << projectName << "created.";
     } else {
-        const QString     &projectName = cmdArguments.first();
-        QSocProjectManager projectManager;
-        if (parser.isSet("path")) {
-            projectManager.setProjectPath(parser.value("path"));
+        qCritical() << "Error: failed to create project" << projectName;
+    }
+}
+
+void QSocCliWorker::parseProjectUpdate(const QStringList &appArguments)
+{
+    /* Clear upstream positional arguments and setup subcommand */
+    parser.clearPositionalArguments();
+    parser.addOptions({
+        {{"p", "path"},
+         QCoreApplication::translate("main", "The path to the project directory."),
+         "project path"},
+        {{"b", "bus"},
+         QCoreApplication::translate("main", "The path to the bus directory."),
+         "bus directory"},
+        {{"m", "sym"},
+         QCoreApplication::translate("main", "The path to the symbol directory."),
+         "symbol directory"},
+        {{"s", "sch"},
+         QCoreApplication::translate("main", "The path to the schematic directory."),
+         "schematic directory"},
+        {{"o", "output"},
+         QCoreApplication::translate("main", "The path to the output file."),
+         "output directory"},
+    });
+    parser.addPositionalArgument(
+        "name",
+        QCoreApplication::translate("main", "The name of the project to be update."),
+        "[<name>]");
+
+    parser.parse(appArguments);
+    const QStringList cmdArguments = parser.positionalArguments();
+    if (cmdArguments.isEmpty()) {
+        if (!parser.isSet("help")) {
+            qCritical() << "Error: missing project name.";
+            parser.showHelp(1);
+        } else {
+            parser.showHelp(0);
         }
+        return;
+    }
+    /* Pass projectName to projectManager */
+    const QString     &projectName = cmdArguments.first();
+    QSocProjectManager projectManager;
+    if (parser.isSet("path")) {
+        projectManager.setProjectPath(parser.value("path"));
+    }
+    /* Load project by name from project path */
+    if (projectManager.load(projectName)) {
+        qInfo() << "Project" << projectName << "loaded.";
+        /* Update project config with new paths */
         if (parser.isSet("bus")) {
             projectManager.setBusPath(parser.value("bus"));
         }
@@ -101,14 +169,15 @@ void QSocCliWorker::parseProjectCreate(const QStringList &appArguments)
         if (parser.isSet("output")) {
             projectManager.setOutputPath(parser.value("output"));
         }
+        /* Save project config */
         if (projectManager.save(projectName)) {
-            qInfo() << "Project" << projectName << "created.";
+            qInfo() << "Project" << projectName << "saved.";
         } else {
-            qCritical() << "Error: failed to create project" << projectName;
+            qCritical() << "Error: failed to save project" << projectName;
         }
+    } else {
+        qCritical() << "Error: failed to load project" << projectName;
     }
 }
-
-void QSocCliWorker::parseProjectUpdate(const QStringList &appArguments) {}
 
 void QSocCliWorker::parseProjectRemove(const QStringList &appArguments) {}
