d0330241a513c6139581355c152ef8c71520999a
Huang Rui <vowstar@gmail.com>
2023-06-13 12:44:14 +0800

    MainWindow: documenting the code
    Signed-off-by: Huang Rui <vowstar@gmail.com>

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -8,16 +8,30 @@ namespace Ui {
 class MainWindow;
 }
 QT_END_NAMESPACE
-
+/**
+ * @brief   The MainWindow class
+ * @details This class is the main window class for the socstudio application.
+ *          It is responsible for displaying the main window.
+ */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructor for MainWindow
+     * @param parent parent object
+     * @details This constructor will initialize the main window
+     */
     MainWindow(QWidget *parent = nullptr);
+    /**
+     * @brief Destructor for MainWindow
+     * @details This destructor will free the main window
+     */
     ~MainWindow();
 
 private:
+    /* Main window UI */
     Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
