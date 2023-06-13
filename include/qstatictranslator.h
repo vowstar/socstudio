d4953537d6cf7e43fa4deb364eb541b9635e2285
Huang Rui <vowstar@gmail.com>
2023-06-13 12:27:47 +0800

    QStaticTranslator: add documentation comments
    Signed-off-by: Huang Rui <vowstar@gmail.com>

--- include/qstatictranslator.h
+++ include/qstatictranslator.h
@@ -4,10 +4,16 @@
 #include <QObject>
 #include <QTranslator>
 
+/**
+ * @brief   The QStaticTranslator class
+ * @details This class is used to setup the translator for the application.
+ *          It will load the translation files from the resource file.
+ */
 class QStaticTranslator : public QObject
 {
     Q_OBJECT
-
+public:
+    /* Static instance of this object */
     static QStaticTranslator &instance()
     {
         static QStaticTranslator instance;
@@ -15,15 +21,27 @@ class QStaticTranslator : public QObject
     }
 
 public slots:
-    static void setup(void);
+    /**
+     * @brief Setup the translator
+     * @details This function will setup the translator for the application.
+     *          It will load the translation files from the resource file.
+     */
+    static void setup();
 
 private:
     /* Disallow creating an instance of this object from external */
     QStaticTranslator(){};
-
+    /* App translator */
     static QTranslator translator;
+    /* QT Base translator */
     static QTranslator translatorBase;
-
+    /**
+     * @brief Initialize the translator
+     * @param translator translator object
+     * @param prefix prefix of the translation files
+     * @details This function will initialize the translator for the application.
+     *          It will load the translation files from the resource file.
+     */
     static void initTranslator(QTranslator &translator, const QString &prefix = ":/i18n/");
 };
 
